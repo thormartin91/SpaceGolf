a9713206dae82471d45f924129380a5de3754e76
larsfk <larsklingen@gmail.com>
2015-03-03 16:34:02 +0100

    Neste fix. Sliter med applyImpulse().

--- SpaceGolf/SpaceGolf/GameScene.swift
+++ SpaceGolf/SpaceGolf/GameScene.swift
@@ -9,35 +9,61 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var startPoint = CGPoint()
+    var endPoint = CGPoint()
+    let ball = SKSpriteNode(imageNamed:"ball.png")
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+
         
-        self.addChild(myLabel)
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: view.frame)
+        self.physicsWorld.gravity = CGVectorMake(0, 0)
+        
+        
+        let ball = SKSpriteNode(imageNamed:"ball.png")
+        let location = CGPoint(x: self.frame.height / 2 , y: self.frame.width/2)
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
+        ball.physicsBody?.dynamic = true
+        ball.physicsBody?.velocity = CGVectorMake(0, 0)
+        ball.physicsBody?.applyForce(CGVectorMake(0, 280))
+        
+        ball.position = location
+        ball.setScale(0.1)
+        
+        self.addChild(ball)
+        
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            startPoint = touch.locationInNode(self)
+            println("Start: \(startPoint)")
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
     }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+                endPoint = touch.locationInNode(self)
+                println("End: \(endPoint)")
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        var shootVector = CGVectorMake(endPoint.x - startPoint.x, endPoint.y - startPoint.y)
+        
+        println("Vector: \(shootVector.dx),\(shootVector.dy)")
+        
+        ball.physicsBody?.applyImpulse(shootVector)
+    }
+    
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
