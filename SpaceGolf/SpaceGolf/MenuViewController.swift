9875f04e18b16dd892c499ebb597b15f2c1fa828
Thor Martin Abrahamsen <thormartin@dhcp-078194.fa-s.ntnu.no>
2015-03-04 18:16:47 +0100

    add viewDidLoad and memoryWarning

--- SpaceGolf/SpaceGolf/MenuViewController.swift
+++ SpaceGolf/SpaceGolf/MenuViewController.swift
@@ -6,4 +6,19 @@
 //  Copyright (c) 2015 Progark. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do additional setup after loading the view
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
