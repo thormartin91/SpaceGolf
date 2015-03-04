188f32775757afc7927baac1a4c4a83132553256
JonasAD <jonas.dalseth@gmail.com>
2015-03-04 18:07:03 +0100

    Make leaderboardclass

---
+++ SpaceGolf/SpaceGolf/Leaderboard.swift
@@ -0,0 +1,13 @@
+//
+//  Leaderboard.swift
+//  SpaceGolf
+//
+//  Created by Jonas André Dalseth on 04.03.15.
+//  Copyright (c) 2015 Progark. All rights reserved.
+//
+
+import Foundation
+
+class Leaderboard {
+    
+}
