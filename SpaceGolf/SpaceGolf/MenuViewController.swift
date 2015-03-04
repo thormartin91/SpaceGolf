3a53566ac90693f4c48b19d3b5ba7cab2db5a2dd
Thor Martin Abrahamsen <thormartin@dhcp-078194.fa-s.ntnu.no>
2015-03-04 17:58:31 +0100

    add MenuViewController.swift

---
+++ SpaceGolf/SpaceGolf/MenuViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MenuViewController.swift
+//  SpaceGolf
+//
+//  Created by Thor Martin Abrahamsen on 04.03.15.
+//  Copyright (c) 2015 Progark. All rights reserved.
+//
+
+import Foundation
