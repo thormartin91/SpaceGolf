d53193cb172015505012789f8ca4d653474837b0
JonasAD <jonas.dalseth@gmail.com>
2015-03-04 16:49:39 +0100

    Made player class
    Made player class, and added functions for changing the score, and
    register hits.

---
+++ SpaceGolf/SpaceGolf/Player.swift
@@ -0,0 +1,32 @@
+//
+//  Player.swift
+//  SpaceGolf
+//
+//  Created by Jonas André Dalseth on 04.03.15.
+//  Copyright (c) 2015 Progark. All rights reserved.
+//
+
+import Foundation
+import GameKit
+
+class Player: GKLocalPlayer {
+    private var score: Double
+    private var hits: Int
+    private var ball: Ball
+    
+    override init(){
+        score = 0
+        hits = 0
+        ball = Ball()
+    }
+    
+    func changeScore(deltaScore: Double){
+        score += deltaScore;
+    }
+    
+    func addHit(){
+        hits++
+    }
+    
+    
+}
