27b852a7bd1a1918413851e1c0065195ffa5be2e
thormartin91 <thorma91@gmail.com>
2015-03-06 16:31:25 +0100

    add functonality to add playernames

--- SpaceGolf/SpaceGolf/AddPlayersViewController.swift
+++ SpaceGolf/SpaceGolf/AddPlayersViewController.swift
@@ -6,4 +6,31 @@
 //  Copyright (c) 2015 Progark. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SpriteKit
+
+class AddPlayersViewController: UIViewController {
+    
+    @IBOutlet weak var playerTextField: UITextField!
+    @IBOutlet weak var playersTextView: UITextView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do additional setup after loading the view
+        playerTextField.placeholder = "Enter name..."
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addPlayerButton(sender: AnyObject) {
+        var existingPlayers = playersTextView.text
+        playersTextView.text = playerTextField.text + "\n" + existingPlayers
+        playerTextField.text = ""
+
+    }
+    
+}
--- SpaceGolf/SpaceGolf/GameMenuViewController.swift
+++ SpaceGolf/SpaceGolf/GameMenuViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+class GameMenuViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
