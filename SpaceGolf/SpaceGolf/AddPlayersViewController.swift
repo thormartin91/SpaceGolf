8d37395057ecd278387300376e59f756d2c25c85
thormartin91 <thorma91@gmail.com>
2015-03-06 15:38:12 +0100

    create AddPlayersViewController

---
+++ SpaceGolf/SpaceGolf/AddPlayersViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddPlayersViewController.swift
+//  SpaceGolf
+//
+//  Created by Thor Martin Abrahamsen on 06.03.15.
+//  Copyright (c) 2015 Progark. All rights reserved.
+//
+
+import Foundation
